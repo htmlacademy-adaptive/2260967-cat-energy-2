bc5ac7dce3d003837958914c4bae66f1970381e4
Потапова Юлия <potapovayulia789@yandex.ru>
2025-10-02 18:12:19 +0300

    исправляет бэм

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,26 +3,26 @@ const navMain = document.querySelector('header');
 const navToggle = document.querySelector('.header-wrapper__toggle');
 const navig = document.querySelector('.main-nav');
 const toggle = document.querySelector('.header-wrapper__toglewrap');
-const mainmapimg = document.querySelector('.main-map--no-js');
-const mainmapframe = document.querySelector('.main-map--frame-hidden');
+const mainmapimg = document.querySelector('.main-map__picture--nojs');
+const mainmapframe = document.querySelector('.main-map__frame--hidden');
 
 navig.classList.remove('main-nav--nojs');
 toggle.classList.remove('header-wrapper__toglewrap--nojs');
-mainmapimg.classList.remove('main-map--no-js');
+mainmapimg.classList.remove('main-map__picture--nojs');
 mainmapimg.classList.add('main-map--js');
-mainmapframe.classList.remove('main-map--frame-hidden');
+mainmapframe.classList.remove('main-map__frame--hidden');
 mainmapframe.classList.add('main-map--frame-shown');
 
 navMain.classList.add('header-wrapper--closed');
-navMain.classList.remove('header-wrapper--opened');
+navMain.classList.remove('main-header--opened');
 
 navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('header-wrapper--closed')) {
     navMain.classList.remove('header-wrapper--closed');
-    navMain.classList.add('header-wrapper--opened');
+    navMain.classList.add('main-header--opened');
   } else {
     navMain.classList.add('header-wrapper--closed');
-    navMain.classList.remove('header-wrapper--opened');
+    navMain.classList.remove('main-header--opened');
   }
 });
 
