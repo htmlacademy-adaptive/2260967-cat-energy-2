1ef915a1ff43bba60c10c22421c6bd64f9c1b5be
Потапова Юлия <potapovayulia789@yandex.ru>
2025-10-02 15:39:12 +0300

    Исправления линтнера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,11 +1,11 @@
 /* в этот файл добавляет скрипты*/
-let navMain = document.querySelector('header');
-let navToggle = document.querySelector('.header-wrapper__toggle');
-let navig = document.querySelector('.main-nav');
-let nojs = document.querySelector('.main-nav--nojs');
-let toggle = document.querySelector('.header-wrapper__toglewrap');
-let mainmapimg = document.querySelector('.main-map--no-js');
-let mainmapframe = document.querySelector('.main-map--frame-hidden');
+const navMain = document.querySelector('header');
+const navToggle = document.querySelector('.header-wrapper__toggle');
+const navig = document.querySelector('.main-nav');
+const nojs = document.querySelector('.main-nav--nojs');
+const toggle = document.querySelector('.header-wrapper__toglewrap');
+const mainmapimg = document.querySelector('.main-map--no-js');
+const mainmapframe = document.querySelector('.main-map--frame-hidden');
 
 navig.classList.remove('main-nav--nojs');
 toggle.classList.remove('header-wrapper__toglewrap--nojs');
@@ -17,7 +17,7 @@ mainmapframe.classList.add('main-map--frame-shown');
 navMain.classList.add('header-wrapper--closed');
 navMain.classList.remove('header-wrapper--opened');
 
-navToggle.addEventListener('click', function () {
+navToggle.addEventListener('click', () => {
   if (navMain.classList.contains('header-wrapper--closed')) {
     navMain.classList.remove('header-wrapper--closed');
     navMain.classList.add('header-wrapper--opened');
